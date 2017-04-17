22393802c3a8bd5ea7f39e491a942b194ddc540a
Everton Yoshitani <everton@notreve.com>
2017-04-17 16:06:54 +0900

    Router improvements
    - Adds support for more verbs
    - Adds a runner test util module to help initialise instances of `eventData`
    - Adds more `router` tests

--- test/router-spec.js
+++ test/router-spec.js
@@ -1,46 +1,11 @@
 'use strict';
 
+
 describe('Router', function() {
 
-	var router;
+	var util = require('./support/runner-test-util');
 
-	var fakeEventData = {
-		request: {
-			headers: null,
-			body: null,
-			method: null,
-			params: {
-				workspaceId: null,
-				pluginNamespace: null,
-				pluginRoute: null
-			},
-			query: null,
-			originalUrl: null
-		},
-		response: {
-			set: function(field, value) {
-				// if (arguments.length === 2) {
-				// 	res.set(field, value);
-				// } else {
-				// 	res.set(field);
-				// }
-				return this;
-			},
-			status: function(status) {
-				// res.status(status);
-				return this;
-			},
-			send: function(body) {
-				// res.send(body);
-				return body;
-				// return this;
-			},
-			end: function() {
-				// res.end();
-				return this;
-			}
-		}
-	};
+	var router;
 
 	beforeEach(function() {
 		router = require('../index').router;
@@ -50,8 +15,10 @@ describe('Router', function() {
 
 		it('should accept GET verb', function() {
 
-			fakeEventData.request.method = 'GET';
-			fakeEventData.request.originalUrl = '/1/2/3/4/two';
+			var eventData = util.createEventData({
+				method: 'GET',
+				originalUrl: '/1/2/3/4/two'
+			});
 
 			var testActionOne = function() {};
 
@@ -65,7 +32,7 @@ describe('Router', function() {
 
 			router.get('/two', testActionTwo);
 
-			router.dispatch(fakeEventData).then(function(results) {
+			return router.dispatch(eventData).then(function(results) {
 				expect(results).to.equal('testGET');
 			});
 
@@ -73,8 +40,10 @@ describe('Router', function() {
 
 		it('should accept POST verb', function() {
 
-			fakeEventData.request.method = 'POST';
-			fakeEventData.request.originalUrl = '/1/2/3/4/two';
+			var eventData = util.createEventData({
+				method: 'POST',
+				originalUrl: '/1/2/3/4/two'
+			});
 
 			var testActionOne = function() {};
 
@@ -88,8 +57,83 @@ describe('Router', function() {
 
 			router.post('/two', testActionTwo);
 
-			router.dispatch(fakeEventData).then(function(results) {
+			return router.dispatch(eventData).then(function(results) {
 				expect(results).to.equal('testPOST');
+			});
+
+		});
+
+		it('should accept PUT verb', function() {
+
+			var eventData = util.createEventData({
+				method: 'PUT',
+				originalUrl: '/1/2/3/4/two'
+			});
+
+			var testActionOne = function() {};
+
+			var testActionTwo = function() {
+				return new Promise(function(resolve, reject) {
+					resolve('testPUT');
+				});
+			};
+
+			router.put('/one', testActionOne);
+
+			router.put('/two', testActionTwo);
+
+			return router.dispatch(eventData).then(function(results) {
+				expect(results).to.equal('testPUT');
+			});
+
+		});
+
+		it('should accept DELETE verb', function() {
+
+			var eventData = util.createEventData({
+				method: 'DELETE',
+				originalUrl: '/1/2/3/4/two'
+			});
+
+			var testActionOne = function() {};
+
+			var testActionTwo = function() {
+				return new Promise(function(resolve, reject) {
+					resolve('testDELETE');
+				});
+			};
+
+			router.delete('/one', testActionOne);
+
+			router.delete('/two', testActionTwo);
+
+			return router.dispatch(eventData).then(function(results) {
+				expect(results).to.equal('testDELETE');
+			});
+
+		});
+
+		it('should accept PATCH verb', function() {
+
+			var eventData = util.createEventData({
+				method: 'PATCH',
+				originalUrl: '/1/2/3/4/two'
+			});
+
+			var testActionOne = function() {};
+
+			var testActionTwo = function() {
+				return new Promise(function(resolve, reject) {
+					resolve('testPATCH');
+				});
+			};
+
+			router.patch('/one', testActionOne);
+
+			router.patch('/two', testActionTwo);
+
+			return router.dispatch(eventData).then(function(results) {
+				expect(results).to.equal('testPATCH');
 			});
 
 		});
@@ -98,9 +142,157 @@ describe('Router', function() {
 
 	describe('error handling', function() {
 
-	});
+		it('should respond with a 404 not found for inexisting route', function() {
 
-	describe('routing', function() {
+			var eventData = util.createEventData({
+				method: 'GET',
+				originalUrl: '/1/2/3/4/not-setup-route'
+			});
+
+			var testActionOne = function() {};
+
+			var testActionTwo = function() {};
+
+			router.get('/one', testActionOne);
+
+			router.get('/two', testActionTwo);
+
+			var expected = {
+				name: 'NotFound',
+				message: 'Not Found'
+			};
+
+			var results = router.dispatch(eventData);
+
+			expect(results).to.deep.equal(expected);
+
+			return;
+
+		});
+
+		it('should respond with a 500 custom error when promise is rejected', function() {
+
+			var eventData = util.createEventData({
+				method: 'GET',
+				originalUrl: '/1/2/3/4/two'
+			});
+
+			var testActionOne = function() {};
+
+			var testActionTwo = function() {
+				return new Promise(function(resolve, reject) {
+					reject(new Error('Test Error'));
+				});
+			};
+
+			router.get('/one', testActionOne);
+
+			router.get('/two', testActionTwo);
+
+			var expected = {
+				name: 'Error',
+				message: 'Test Error'
+			};
+
+			return router.dispatch(eventData).then(function(results) {
+				expect(results.name).to.equal(expected.name);
+				expect(results.message).to.equal(expected.message);
+			});
+
+		});
+
+		it('should respond with a 500 default error when promise is rejected', function() {
+
+			var eventData = util.createEventData({
+				method: 'GET',
+				originalUrl: '/1/2/3/4/two'
+			});
+
+			var testActionOne = function() {};
+
+			var testActionTwo = function() {
+				return new Promise(function(resolve, reject) {
+					reject();
+				});
+			};
+
+			router.get('/one', testActionOne);
+
+			router.get('/two', testActionTwo);
+
+			var expected = {
+				name: 'InternalServerError',
+				message: 'Internal Server Error'
+			};
+
+			return router.dispatch(eventData).then(function(results) {
+				expect(results.name).to.equal(expected.name);
+				expect(results.message).to.equal(expected.message);
+			});
+
+		});
+
+		it('should respond with a 500 custom error when a exception is throwed', function() {
+
+			var eventData = util.createEventData({
+				method: 'GET',
+				originalUrl: '/1/2/3/4/two'
+			});
+
+			var testActionOne = function() {};
+
+			var testActionTwo = function() {
+				return new Promise(function(resolve, reject) {
+					throw new Error('Test Error');
+				});
+			};
+
+			router.get('/one', testActionOne);
+
+			router.get('/two', testActionTwo);
+
+			var expected = {
+				name: 'Error',
+				message: 'Test Error'
+			};
+
+			return router.dispatch(eventData).then(function(results) {
+				expect(results.name).to.equal(expected.name);
+				expect(results.message).to.equal(expected.message);
+			});
+
+		});
+
+		it('should respond with a 500 default error when a exception is throwed', function() {
+
+			var eventData = util.createEventData({
+				method: 'GET',
+				originalUrl: '/1/2/3/4/two'
+			});
+
+			var testActionOne = function() {};
+
+			var testActionTwo = function() {
+				return new Promise(function(resolve, reject) {
+					throw {};
+				});
+			};
+
+			router.get('/one', testActionOne);
+
+			router.get('/two', testActionTwo);
+
+			var expected = {
+				name: 'InternalServerError',
+				message: 'Internal Server Error'
+			};
+
+			return router.dispatch(eventData).then(function(results) {
+				expect(results.name).to.equal(expected.name);
+				expect(results.message).to.equal(expected.message);
+			});
+
+		});
 
 	});
 
