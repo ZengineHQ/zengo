d63c7f59c585995bff748dab3a5a11ccd04d0a18
Everton Yoshitani <everton@notreve.com>
2017-04-28 05:06:27 +0900

    Adds `count` public API

--- src/record-dao.js
+++ src/record-dao.js
@@ -35,6 +35,10 @@ var RecordDao = function(formDao, recordDaoRaw, formId) {
 		return recordDaoRaw.save(request);
 	};
 
+	dao.count = function(request) {
+		return recordDaoRaw.count(request);
+	};
+
 	return dao;
 };
 
