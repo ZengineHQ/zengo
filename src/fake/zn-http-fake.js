3fd544b70875ae1f907e24a754ef0c682e133dca
Roberto Carraretto <rc.carraretto@gmail.com>
2017-04-27 22:18:35 -0300

    zn http fake: improve route pattern matching

--- src/fake/zn-http-fake.js
+++ src/fake/zn-http-fake.js
@@ -1,64 +1,97 @@
 'use strict';
 
 var Promise = require('bluebird');
+var RoutePattern = require('route-pattern');
+var find = require('lodash.find');
+var merge = require('lodash.merge');
 
 var httpFakeForm = require('./zn-http-fake-form.js');
 var httpFakeRecord = require('./zn-http-fake-record.js');
 
-var createZnHttpFake = function() {
-	var znHttp = {};
 
+var createGenericRouter = function() {
+
+	var actions = {
+		'GET': [],
+		'POST': [],
+	};
+
+	var register = function(method, pattern, callback) {
+		var route = {
+			pattern: pattern,
+			callback: callback,
+		};
+		actions[method].push(route);
+	};
+
+	var registerGetAction = function(pattern, callback) {
+		return register('GET', pattern, callback);
+	};
+
+	var registerPostAction = function(pattern, callback) {
+		return register('POST', pattern, callback);
+	};
+
+	var dispatch = function(method, endpoint, params) {
+		var route = find(actions[method], function(route) {
+			return RoutePattern.fromString(route.pattern).matches(endpoint);
+		});
+		var match = RoutePattern.fromString(route.pattern).match(endpoint);
+		var allParams = merge(params, match.namedParams);
+		return route.callback(allParams);
+	};
+
+	return {
+		get: registerGetAction,
+		post: registerPostAction,
+		dispatch: dispatch,
+	};
+};
+
+var createZnCoreFake = function() {
+
+	var router = createGenericRouter();
 	var fakeFormRepo = httpFakeForm();
 	var fakeRecordRepo = httpFakeRecord();
 
-	var dispatchGetFormOrRecord = function(tokens, params) {
-		if (!tokens.length) {
-			return fakeFormRepo.query(params);
-		}
-		var formId = parseInt(tokens[0]);
-		if (tokens.length === 1) {
-			return fakeFormRepo.get(formId);
-		}
-		if (tokens.length === 2) {
-			return fakeRecordRepo.forForm(formId).query();
-		}
-	};
+	router.get('/forms', function(params) {
+		return fakeFormRepo.query(params);
+	});
 
-	var dispatchGet = function(endpoint, params) {
-		var tokens = endpoint.split('/');
-		tokens.shift();
-		var resource = tokens.shift();
-		if (resource === 'forms') {
-			return dispatchGetFormOrRecord(tokens, params);
-		}
-		return [];
-	};
+	router.post('/forms', function(data) {
+		return fakeFormRepo.save(data);
+	});
 
-	var dispatchPostFormOrRecord = function(tokens, data) {
-		if (!tokens.length) {
-			return fakeFormRepo.save(data);
-		}
-		var formId = parseInt(tokens[0]);
-		if (tokens.length === 1) {
-			return;
-		}
-		if (tokens.length === 2) {
-			return fakeRecordRepo.forForm(formId).save(data);
-		}
-	};
+	router.get('/forms/:formId', function(data) {
+		var formId = parseInt(data.formId);
+		delete data.formId;
+		return fakeFormRepo.get(formId);
+	});
 
-	var dispatchPost = function(endpoint, data) {
-		var tokens = endpoint.split('/');
-		tokens.shift();
-		var resource = tokens.shift();
-		if (resource === 'forms') {
-			return dispatchPostFormOrRecord(tokens, data);
-		}
-		return [];
+	router.get('/forms/:formId/records', function(data) {
+		var formId = parseInt(data.formId);
+		delete data.formId;
+		return fakeRecordRepo.forForm(formId).query();
+	});
+
+	router.post('/forms/:formId/records', function(data) {
+		var formId = parseInt(data.formId);
+		delete data.formId;
+		return fakeRecordRepo.forForm(formId).save(data);
+	});
+
+	return {
+		dispatch: router.dispatch,
 	};
+};
+
+var createZnHttpFake = function() {
+	var znHttp = {};
+
+	var core = createZnCoreFake();
 
 	znHttp.get = function(endpoint, options) {
-		var data = dispatchGet(endpoint, options.params);
+		var data = core.dispatch('GET', endpoint, options.params);
 		var body = {
 			data: data,
 			totalCount: data.length || 0,
@@ -74,7 +107,7 @@ var createZnHttpFake = function() {
 	};
 
 	znHttp.post = function(endpoint, data) {
-		var responseData = dispatchPost(endpoint, data);
+		var responseData = core.dispatch('POST', endpoint, data);
 		var body = {
 			data: responseData,
 		};
